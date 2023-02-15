53860cca8e083c661e18b66b637b00f7d522e6be
ITOH Tsubasa <1021wing@gmail.com>
2023-02-15 18:00:27 +0900

    Support alpha channel

--- RGBColorPickerDialog/src/main/java/app/web/knot3home/rgbcolorpickerdialog/RGBColorPickerDialog.kt
+++ RGBColorPickerDialog/src/main/java/app/web/knot3home/rgbcolorpickerdialog/RGBColorPickerDialog.kt
@@ -16,19 +16,41 @@ class RGBColorPickerDialog : DialogFragment() {
     private var r = 0
     private var g = 0
     private var b = 0
+    private var a = 255
+    private var isAlphaEnabled = false
     private var _listener: Listener? = null
 
     interface Listener {
-        fun onFinished(r: Int, g: Int, b: Int)
+        fun onFinished(r: Int, g: Int, b: Int, a: Int)
     }
 
     companion object {
         fun create(r: Int, g: Int, b: Int): RGBColorPickerDialog {
+            if(r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255){
+                throw IllegalArgumentException("r/g/b must be 0..255")
+            }
+
             return RGBColorPickerDialog().apply {
                 val args = Bundle()
                 args.putInt("key_r", r)
                 args.putInt("key_g", g)
                 args.putInt("key_b", b)
+                args.putBoolean("key_alpha", false)
+                this.arguments = args
+            }
+        }
+        fun create(r: Int, g: Int, b: Int, a: Int): RGBColorPickerDialog {
+            if(r < 0 || r > 255 || g < 0 || g > 255 || b < 0 || b > 255 || a < 0 || a > 255){
+                throw IllegalArgumentException("r/g/b/a must be 0..255")
+            }
+
+            return RGBColorPickerDialog().apply {
+                val args = Bundle()
+                args.putInt("key_r", r)
+                args.putInt("key_g", g)
+                args.putInt("key_b", b)
+                args.putBoolean("key_alpha", true)
+                args.putInt("key_a", a)
                 this.arguments = args
             }
         }
@@ -43,23 +65,37 @@ class RGBColorPickerDialog : DialogFragment() {
             r = it.getInt("key_r", 0)
             g = it.getInt("key_g", 0)
             b = it.getInt("key_b", 0)
+            isAlphaEnabled = it.getBoolean("key_alpha", false)
+            if(isAlphaEnabled){
+                a = it.getInt("key_a", 0)
+            }
         }
         val factory = LayoutInflater.from(requireContext())
         val dialogView = factory.inflate(R.layout.rgb_color_picker_dialog, null)
+
+        val alphaValueTextView = dialogView.findViewById<TextView>(R.id.alpha_value)
+        val alphaSeekBar = dialogView.findViewById<SeekBar>(R.id.seekBarA)
+        if(isAlphaEnabled){
+            dialogView.findViewById<TextView>(R.id.label_a).visibility = View.VISIBLE
+            alphaValueTextView.visibility = View.VISIBLE
+            alphaSeekBar.visibility = View.VISIBLE
+        }
 
         fun refresh() {
             dialogView.findViewById<TextView>(R.id.red_value).text = r.toString()
             dialogView.findViewById<TextView>(R.id.green_value).text = g.toString()
             dialogView.findViewById<TextView>(R.id.blue_value).text = b.toString()
+            alphaValueTextView.text = a.toString()
 
             dialogView.findViewById<View>(R.id.color_preview).setBackgroundColor(
-                Color.rgb(r, g, b)
+                Color.argb(a,r,g,b)
             )
         }
 
         dialogView.findViewById<SeekBar>(R.id.seekBarR).progress = r
         dialogView.findViewById<SeekBar>(R.id.seekBarG).progress = g
         dialogView.findViewById<SeekBar>(R.id.seekBarB).progress = b
+        alphaSeekBar.progress = a
         refresh()
 
         val listener = object : OnSeekBarChangeListener {
@@ -74,6 +110,9 @@ class RGBColorPickerDialog : DialogFragment() {
                     R.id.seekBarB -> {
                         b = progress
                     }
+                    R.id.seekBarA -> {
+                        a = progress
+                    }
                 }
                 refresh()
             }
@@ -87,11 +126,12 @@ class RGBColorPickerDialog : DialogFragment() {
         dialogView.findViewById<SeekBar>(R.id.seekBarR).setOnSeekBarChangeListener(listener)
         dialogView.findViewById<SeekBar>(R.id.seekBarG).setOnSeekBarChangeListener(listener)
         dialogView.findViewById<SeekBar>(R.id.seekBarB).setOnSeekBarChangeListener(listener)
+        alphaSeekBar.setOnSeekBarChangeListener(listener)
 
         val dialog = AlertDialog.Builder(requireActivity())
             .setView(dialogView)
             .setPositiveButton(getString(R.string.ok)) { _, _ ->
-                _listener?.onFinished(r, g, b)
+                _listener?.onFinished(r, g, b, a)
             }
             .create()
 
--- example/src/main/java/app/web/knot3home/rgbColorPickerExample/MainActivity.kt
+++ example/src/main/java/app/web/knot3home/rgbColorPickerExample/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.core.graphics.alpha
 import androidx.core.graphics.red
 import androidx.core.graphics.green
 import androidx.core.graphics.blue
@@ -25,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             // Call the dialog
             val dialog = RGBColorPickerDialog.create(color.red, color.green, color.blue)
             val listener: RGBColorPickerDialog.Listener = object : RGBColorPickerDialog.Listener {
-                override fun onFinished(r: Int, g: Int, b: Int) {
+                override fun onFinished(r: Int, g: Int, b: Int, a: Int) {
                     color = Color.rgb(r, g, b)
                     colorView.setBackgroundColor(color)
                 }
@@ -34,5 +35,19 @@ class MainActivity : AppCompatActivity() {
             dialog.setOnFinishedListener(listener)
             dialog.show(supportFragmentManager, "dialog")
         }
+
+        findViewById<Button>(R.id.pickButton2).setOnClickListener {
+            val dialog = RGBColorPickerDialog.create(color.red, color.green, color.blue, color.alpha)
+            val listener: RGBColorPickerDialog.Listener = object : RGBColorPickerDialog.Listener {
+                override fun onFinished(r: Int, g: Int, b: Int, a: Int) {
+                    color = Color.argb(a, r, g, b)
+                    colorView.setBackgroundColor(color)
+                }
+            }
+
+            dialog.setOnFinishedListener(listener)
+            dialog.show(supportFragmentManager, "dialog")
+        }
+
     }
 }
